2ef9e444d5a15f5371eccd4e01167ee0cd85e485
Dmitry Likane <dmitry.likane@anz.com>
2022-11-07 09:25:53 +1100

    add allowances to timeshift

--- cmd/timeshift/timeshift.go
+++ cmd/timeshift/timeshift.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"time"
+
+	"github.com/jszwec/csvutil"
 )
 
 type xmlTime time.Time
@@ -56,18 +59,26 @@ type Allowance struct {
 }
 
 type TimeCard struct {
-	XMLName      xml.Name `xml:"TimeCard"`
-	TimeCardNo   string   `xml:"TimeCardNo"`
-	EmployeeName string   `xml:"EmployeeName"`
-	Shift        []Shift  `xml:"Shift"`
+	XMLName      xml.Name   `xml:"TimeCard"`
+	TimeCardNo   string     `xml:"TimeCardNo"`
+	EmployeeName string     `xml:"EmployeeName"`
+	Shift        []Shift    `xml:"Shift"`
 	Allowance    *Allowance `xml:"Allowance,omitempty"`
-	TotalHours   float32  `xml:"TotalHours"`
+	TotalHours   float32    `xml:"TotalHours"`
 }
 
 type TimeCards struct {
 	XMLName   xml.Name   `xml:"TimeCards"`
 	Version   string     `xml:"Version,attr"`
 	TimeCards []TimeCard `xml:"TimeCard"`
+}
+
+type CsvShift struct {
+	FullName  string  `csv:"Full Name"`
+	Date      string  `csv:"Date"`
+	Shift     string  `csv:"Shift"`
+	Break     float64 `csv:"Break"`
+	Allowance string  `csv:"Allowance"`
 }
 
 func main() {
@@ -81,6 +92,7 @@ func main() {
 	fileName := os.Args[1]
 	ext := filepath.Ext(fileName)
 	fileNameOut := fileName[:len(fileName)-len(ext)] + "_out" + ext
+	csvFileNameOut := fileName[:len(fileName)-len(ext)] + "_out" + ",csv"
 
 	timeCards, err := readCards(fileName)
 	if err != nil {
@@ -90,6 +102,16 @@ func main() {
 	err = processCards(timeCards)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("process cards: %v", err))
+	}
+
+	csvShifts, err := processAllowances(timeCards)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("process allowances: %v", err))
+	}
+
+	err = writeCsvShifts(csvShifts, csvFileNameOut)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("writting csvs: %v", err))
 	}
 
 	err = writeCards(timeCards, fileNameOut)
@@ -134,11 +156,7 @@ func processCards(timeCards *TimeCards) error {
 	for i, tc := range timeCards.TimeCards {
 		for j, shift := range tc.Shift {
 			arr := make([]ShiftHours, 0)
-			// currentDay := time.Time{}
 			for _, sh := range shift.ShiftHours {
-				// if ndx == 0 {
-				// 	currentDay = time.Now()
-				// }
 				if sh.Break != nil && *sh.Break != "00:00" {
 					start := time.Time(sh.StartTime)
 					finish := time.Time(sh.FinishTime)
@@ -167,17 +185,111 @@ func processCards(timeCards *TimeCards) error {
 					}
 					arr = append(arr, sh1)
 					arr = append(arr, sh2)
-
-
-
 				} else {
 					sh.Break = nil
 					arr = append(arr, sh)
 				}
 			}
 			timeCards.TimeCards[i].Shift[j].ShiftHours = arr
+
+			sort.Slice(shift.ShiftHours, func(i1 int, i2 int) bool {
+				s1 := shift.ShiftHours[i1]
+				s2 := shift.ShiftHours[i2]
+				return time.Time(s1.StartTime).Before(time.Time(s2.StartTime))
+			})
+		}
+		sort.Slice(tc.Shift, func(i1 int, i2 int) bool {
+			s1 := tc.Shift[i1].ShiftHours[0]
+			s2 := tc.Shift[i2].ShiftHours[0]
+			return time.Time(s1.StartTime).Before(time.Time(s2.StartTime))
+		})
+
+	}
+	return nil
+}
+
+func processAllowances(timeCards *TimeCards) ([]CsvShift, error) {
+	csvShifts := make([]CsvShift, 0)
+	for _, tc := range timeCards.TimeCards {
+		allowances := 0
+		continuesShift := float64(0)
+		shiftNdx := 0
+		var previousShift *ShiftHours
+		isOvernight := false
+		for _, shift := range tc.Shift {
+			for _, sh := range shift.ShiftHours {
+				// is new day
+				if previousShift != nil && truncateToDay(sh.StartTime) != truncateToDay(previousShift.StartTime) {
+					shiftNdx = 0
+				}
+
+				if shiftNdx == 0 {
+					allowances = 0
+					continuesShift = 0
+					previousShift = nil
+					isOvernight = time.Time(sh.StartTime) == truncateToDay(sh.StartTime)
+				}
+				start := time.Time(sh.StartTime)
+				finish := time.Time(sh.FinishTime)
+				shiftDuration := finish.Sub(start).Hours()
+				continuesShift += shiftDuration
+
+				breakDuration := float64(0)
+				if previousShift != nil {
+					breakDuration = time.Time(sh.StartTime).Sub(time.Time(previousShift.FinishTime)).Hours()
+				}
+
+				if shiftNdx == 0 {
+					breakDuration = 0
+				}
+
+				strAllowance := ""
+				// ignore overnight shift (starts at 0:00)
+				if !isOvernight {
+					if breakDuration > 1 || breakDuration > 0 && continuesShift > 5 {
+						if allowances == 0 {
+							strAllowance = "First"
+						} else if allowances == 1 {
+							strAllowance = "Second"
+						}
+						allowances++
+					}
+				}
+
+				csvShifts = append(csvShifts, CsvShift{
+					FullName:  tc.EmployeeName,
+					Date:      printDate(sh.StartTime),
+					Shift:     printStartFinish(sh),
+					Break:     breakDuration,
+					Allowance: strAllowance,
+				})
+
+				shiftNdx++
+				previousShift = &sh
+			}
 		}
 	}
+	return csvShifts, nil
+}
 
-	return nil
+func truncateToDay(x xmlTime) time.Time {
+	t := time.Time(x)
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func printDate(x xmlTime) string {
+	return time.Time(x).Format("2006-02-01")
+}
+func printStartFinish(sh ShiftHours) string {
+	return time.Time(sh.StartTime).Format("15:04") + " - " + time.Time(sh.FinishTime).Format("15:04")
+}
+
+func writeCsvShifts(csvShifts []CsvShift, filename string) error {
+	b, err := csvutil.Marshal(csvShifts)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	bb := []byte("{}\n")
+	bb = append(bb, b...)
+	return ioutil.WriteFile(filename, bb, 0644)
 }
